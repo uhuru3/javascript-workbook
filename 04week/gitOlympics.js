cdbda49fff923b0989163cdf186d9c1c86d90fd1
uhuru3 <uhuru3000@gmail.com>
2018-10-28 13:37:37 -0700

    finished git olymics

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const testArray = ['oly1', 'oly2', 'oly3', 'oly4', 'oly5'];
+
+const printListOfOlympians.forEach = (item, index)=> {
+    console.log(item, index)
+};
+
+printListOfOlympians(testArray);
